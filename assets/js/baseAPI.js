1a02527c9db7862ceae30219740499d0f55c00e2
uioasdzxc0789 <617378601@qq.com>
2022-05-21 10:10:57 +0800

    index

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,18 @@
-$(function () {
-  $.ajaxPrefilter((option) => {
-    option.url = `http://www.liulongbin.top:3007` + option.url
-  })
-})
+// 请求拦截器
+$.ajaxPrefilter((option) => {
+  // 请求之前给有权限的接口注入token
+  if (option.url.includes('/my/')) {
+    option.headers = {
+      Authorization: localStorage.getItem('token')
+    };
+  }
+  option.url = `http://www.liulongbin.top:3007` + option.url;
+  // 统一处理权限问题
+  option.complete = (res) => {
+    console.log(res);
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      localStorage.removeItem('token');
+      location.href = '/login.html';
+    }
+  };
+});
